4a5d78cc9ee0f04d3c0dc301697b050b37090e80
Nicklas <5585060@rts-365.dk>
2021-06-11 00:48:17 +0200

    lavet uendelig loop lavet comments til convertDecimal

--- src/DECBINConverter.java
+++ src/DECBINConverter.java
@@ -5,13 +5,13 @@ public class DECBINConverter {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Would you like to convert to Binary or Decimal. B / C");
+        System.out.println("Would you like to convert to Binary or Decimal. B / D");
         String choice = input.next();
         if(choice.equalsIgnoreCase("b")){
-            System.out.println("Please give 8 digits, the first digit as binary is equals Positive or Minus.");
+            System.out.println("the first digit as binary is equals Positive or Minus.");
             String number = input.next();
             convertBinary(number);
-        }else if (choice.equalsIgnoreCase("c")){
+        }else if (choice.equalsIgnoreCase("d")){
             System.out.println("Please give a decimal number.");
             int number = input.nextInt();
             convertDecimal(number);
@@ -20,18 +20,26 @@ public class DECBINConverter {
 
     public static void convertBinary(String binaryNumber){
         System.out.println(binaryNumber);
-        if(binaryNumber.length() != 8){
-            System.out.println("Not enough bits / too many bits, make sure to use 8 bits");
-        }else {
         int decnum = 0;
 
-        int index = binaryNumber.length() -1;
+        int index = binaryNumber.length() - 1;
 
-        int num = 1;
+        int num = 0;
+        if(Character.getNumericValue(binaryNumber.charAt(0)) == 0){
+            num = 1;
+        }else{
+            num = -1;
+        }
+
 
         // plus værdi
         if(Character.getNumericValue(binaryNumber.charAt(0)) == 0){
             while(index != 0){
+                if(Character.getNumericValue(binaryNumber.charAt(index)) == 1){
+                    decnum = decnum + num;
+                }
+                num = num * 2;
+                /*
                 if(index == 7 && Character.getNumericValue(binaryNumber.charAt(index)) == 1){
                     decnum = decnum + 1;
                 }
@@ -53,10 +61,19 @@ public class DECBINConverter {
                 if(index == 1 && Character.getNumericValue(binaryNumber.charAt(index)) == 1){
                     decnum = decnum + 64;
                 }
+                 */
                 index--;
             }
         }else{
             while(index != 0){
+                // Ones compelement
+
+                if(Character.getNumericValue(binaryNumber.charAt(index)) == 0){
+                    decnum = decnum - num;
+                }
+                num = num * 2;
+
+/*
                 if(index == 7 && Character.getNumericValue(binaryNumber.charAt(index)) == 0){
                     decnum = decnum - 1;
                 }
@@ -78,24 +95,31 @@ public class DECBINConverter {
                 if(index == 1 && Character.getNumericValue(binaryNumber.charAt(index)) == 0){
                     decnum = decnum - 64;
                 }
+
+ */
+
                 index--;
             }
         }
 
+        if(Character.getNumericValue(binaryNumber.charAt(0)) == 1) {
+            System.out.println(Math.negateExact(decnum));
+        }else{
             System.out.println(decnum);
-
         }
 
     }
 
     public static void convertDecimal(int number){
-        System.out.println("test");
         int i = 0;
         int binary[] = new int[100];
 
         // https://youtu.be/rsxT4FfRBaM?t=551
+        // Hvis der er en remainder så vil det blive 1 i binary.
+        // Prøv med lommeregner.
         while(number != 0){
             binary[i]=number%2;
+            //System.out.println(number%2);
             number = number / 2;
             i++;
         }
